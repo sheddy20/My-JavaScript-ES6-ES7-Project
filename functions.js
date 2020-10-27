95d0e46004e69f33770a675ca9c8dffbec0cb521
shedrack <shedrackejeh458@gmail.com>
2020-10-27 22:14:33 +0100

    done

---
+++ functions.js
@@ -0,0 +1,5 @@
+let addNums = (num1, num2) => num1 + num2;
+
+const myNumber = addNums(20, 10);
+console.log(myNumber * 2);
+
--- tenary.js
+++ tenary.js
@@ -1,3 +1,24 @@
 const y = 20;
 
 y === 50 ? console.log(`Y is: ${y}`) : console.log(`Y must be: ${y}`);
+
+const colorPallet = 50;
+
+const color = colorPallet > 50 ? "fuchsia pink" : "deepPurpleAccent";
+
+// console.log(color);
+
+switch (color) {
+    case "fuchsia pink":
+        console.log("color is fuchsia pink");
+        break;
+
+    case "deepPurpleAccent":
+        console.log("color is deepPurpleAccent");
+        break;
+
+    default:
+        console.log("color pallet does not correspond");
+        break;
+}
+
